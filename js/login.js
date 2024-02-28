8efedd8014ad04f6f2e81949d0aed84f2d19ff51
André Moura <andregmoura06@gmail.com>
2024-02-28 18:24:28 -0300

    ate agr pronto

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    // Aqui você pode adicionar lógica para verificar a senha
+    if (username === 'user' && password === 'user123') {
+        // Redireciona para a página após o login bem-sucedido
+        window.location.href = 'character.html';
+    } else {
+        alert('Usuário ou senha inválidos. Por favor, tente novamente.');
+    }
+});
