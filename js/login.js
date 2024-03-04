1b7b6ce977c66ebe4d8643fb7f7d84143a14cea1
André Moura <andregmoura06@gmail.com>
2024-03-04 15:08:22 -0300

    mm

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     // Aqui você pode adicionar lógica para verificar a senha
     if (username === 'user' && password === 'user123') {
         // Redireciona para a página após o login bem-sucedido
-        window.location.href = '../pages/character.html';
+        window.location.href = './pages/character.html';
     } else {
         alert('Usuário ou senha inválidos. Por favor, tente novamente.');
     }
